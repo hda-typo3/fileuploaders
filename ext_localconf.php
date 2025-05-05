7d849d427ff766f8976a8839ac17c75ce52405cf
Michael Lang <115140776+hda-typo3@users.noreply.github.com>
2025-05-05 11:47:04 +0200

    5.5

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,9 @@
 <?php
-if (!defined('TYPO3')) {
-    die('Access denied.');
-}
+declare(strict_types=1);
 
-call_user_func(
-    function()
-    {
-        
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Filelist\FileList::class] = [
-            'className' => AEMKA\Fileuploaders\Xclass\Filelist::class
-        ];
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:filelist/Resources/Private/Language/locallang_mod_file_list.xlf'][] = 'EXT:fileuploaders/Resources/Private/Language/locallang_mod_file_list.xlf';
+defined('TYPO3') or die();
 
-    }
-);
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Filelist\FileList::class] = [
+    'className' => AEMKA\Fileuploaders\Xclass\Filelist::class
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:filelist/Resources/Private/Language/locallang_mod_file_list.xlf'][] = 'EXT:fileuploaders/Resources/Private/Language/locallang_mod_file_list.xlf';
